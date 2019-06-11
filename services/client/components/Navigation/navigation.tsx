854d57c1279b314984a2fa276183909e8a8dbd33
Marcel Fraas <mfraas64@gmail.com>
2019-06-11 21:54:09 +0200

    added logo scroll animation

--- services/client/components/Navigation/navigation.tsx
+++ services/client/components/Navigation/navigation.tsx
@@ -11,40 +11,51 @@ import MaterialIcon from '@material/react-material-icon';
 import './navigation.scss';
 import head from 'next/head';
 import $ from 'jquery';
+import { string } from 'prop-types';
 
 export default () => {
 
-  function vh(v) {
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    return (v * h) / 100;
+
+  function linear_interpolation(toItemSize : number, fromItemSize : number, toReferenceSize : number, fromReferenceSize : number, currentValue : number) {
+    const slope = (toItemSize - fromItemSize) / (toReferenceSize - fromReferenceSize);
+    const base = fromItemSize - slope * fromReferenceSize;
+    return (base + (currentValue * slope))
   }
 
   const handleScroll = e => {
-    //console.log(window.scrollY);
-    const header = document.getElementById('header');
-    const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) * 0.8;
-        header.style.height = `${(h - window.scrollY) < 64 ? 64 : (h - window.scrollY)}px`;
-    header.clientHeight < 65 ? header.classList.add('mdc-top-app-bar--fixed-scrolled-new') : header.classList.remove('mdc-top-app-bar--fixed-scrolled-new'); 
 
+    const h = $('#header').css('max-height').replace('px', '') as unknown as number;
+    $('#header').css('height', `${(h - window.scrollY) < 64 ? 64 : (h - window.scrollY)}px`);
+
+    const size = linear_interpolation(
+      $('#header').css('min-height').replace('px', '') as unknown as number, 
+      $('#logo').css('max-height').replace('px', '') as unknown as number, 
+      ($('#header').css('max-height').replace('px', '') as unknown as number)-100,
+      0, 
+      window.scrollY 
+    )
+
+    $('#logo').css('height', `${size < 52 ? 52 : size}px`);
+    $('#logo').css('width', `${size < 52 ? 52 : size}px`);
+
+    const position = linear_interpolation(
+      6, 
+      (($('#logo_section').css('width').replace('px', '') as unknown as number)-($('#logo').css('max-height').replace('px', '') as unknown as number))/2, 
+      ($('#header').css('max-height').replace('px', '') as unknown as number)-70,
+      0, 
+      window.scrollY 
+    )
+
+    console.log(position);
+
+    $('#logo_section').css('grid-template-columns', `${position < 6 ? 6 : position}px auto 1fr`);
     
-    const grid = $('#logo_section')
-    const logo = $('#logo-container').height()
-    console.log(logo)
-    const fr = ($(window).height()*0.8-256)/2
-    const he = 256 + (Math.round(window.scrollY) * ((256-64) / (0 - vh(80)-64)))
-    const firstRow = 64 + window.scrollY * -($(window). height() * 0.8 - 64) / fr 
-    console.log(`HEIGHT: ${Math.round(he)} | SCROLLED: [${Math.round(window.scrollY)}/${vh(80)-64}]`);
-    //grid.style.gridTemplateRows = `${Math.round(fr - window.scrollY)}px ${Math.round(he)}px ${Math.round(fr - window.scrollY)}px`;
-    grid.css('grid-template-rows', `${Math.max(Math.round(firstRow),0)}px ${Math.round(he)}px 1fr`)
+    $('#header').css('height').replace('px', '') as unknown as number < 65 ? $('#header').addClass('mdc-top-app-bar--fixed-scrolled-new') : $('#header').removeClass('mdc-top-app-bar--fixed-scrolled-new');
 
   }
 
-  
-
-
-
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll, { passive: true });
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -84,9 +95,12 @@ export default () => {
           </TopAppBarSection>
         </TopAppBarRow>
           <TopAppBarSection id="logo_section">
-          <div id="logo-container">
             <div id="logo" className="mdc-toolbar__logo" />
-          </div>
+          </TopAppBarSection>
+          <TopAppBarSection id="title_section">
+            <span id="title">
+              Fachschaft MPI
+            </span>
           </TopAppBarSection>
       </TopAppBar>
       <TopAppBarFixedAdjust>
